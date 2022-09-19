b9c76dc175aba085445846bb80d2e67c26292ac4
arsikz3 <arsen.ziyadinov@gmail.com>
2022-09-19 13:23:37 +0300

    данные получены

---
+++ lib/views/fetch_data.dart
@@ -0,0 +1,26 @@
+import 'dart:convert' as convert;
+
+import 'package:flutter_home_work9/models/hotel.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<HotelPreview>> getData() async {
+  HotelPreview hotel;
+  List<HotelPreview> hotels = [];
+  var url =
+      Uri.https('run.mocky.io', '/v3/ac888dc5-d193-4700-b12c-abb43e289301');
+  var response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    List<dynamic> convertRes = convert.jsonDecode(response.body);
+    for (var element in convertRes) {
+      //print(element);
+
+      hotel = HotelPreview.fromJson(element);
+      hotels.add(hotel);
+    }
+
+    return hotels;
+  } else {
+    return [];
+  }
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_home_work9/models/hotel.dart';
+import 'package:flutter_home_work9/views/fetch_data.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
@@ -8,12 +10,122 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  late Future<List<HotelPreview>> _hotelsPreview;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _hotelsPreview = getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.table_rows,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.grid_view,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+      ),
       body: Center(
-        child: Text("Hotels"),
+        child: FutureBuilder(
+            // future: DefaultAssetBundle.of(context).loadString('assets/data.json'),
+            future: _hotelsPreview,
+            initialData: 'Load Data',
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return const Text('Error');
+                } else if (snapshot.hasData) {
+                  List<HotelPreview>? elem =
+                      snapshot.data as List<HotelPreview>?;
+                  //return Text(elem!.name.toString(), style: const TextStyle(color: Colors.cyan, fontSize: 36));
+
+                  return ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                          child: Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.orangeAccent,
+                            ),
+                            child: Image.asset(
+                              'assets/images/${elem![index].poster}',
+                              fit: BoxFit.cover,
+                              cacheWidth: 100 *
+                                  MediaQuery.of(context)
+                                      .devicePixelRatio
+                                      .round(),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                  width: 180, child: Text(elem![index].name)),
+                              ButtonBar(
+                                children: <Widget>[
+                                  ElevatedButton(
+                                    child: const Text('Подробнее'),
+                                    onPressed: () {/* ... */},
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ));
+                    },
+                    // itemCount: myData == null ? 0 : myData.length,
+                    itemCount: elem!.length,
+                  );
+                } else {
+                  return const Text('Empty data');
+                }
+              } else {
+                return Text('State: ${snapshot.connectionState}');
+              }
+            }
+            // builder: (context, snapshot) {
+            //   if (!snapshot.hasData) {
+            //     return const CircularProgressIndicator();
+            //   }
+            // var myData = json.decode(snapshot.data);
+
+            // return ListView.builder(
+            //   itemBuilder: (BuildContext context, int index) {
+            //     return Card(
+            //          child: ListTile(
+            //        title: Text(myData[index]['table_menu_list'][index]
+            //            ['category_dishes'][index]['dish_name']),
+            //      ));
+            //   },
+            //   // itemCount: myData == null ? 0 : myData.length,
+            //   itemCount: 1,
+            // );
+            //},
+            ),
       ),
     );
   }
